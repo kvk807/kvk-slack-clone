16eb7f8dddbd374d0b1780a8b5eb2c15d0e42693
Kirk Vander Kuy <kvk807@gmail.com>
2021-06-20 15:30:01 -0500

    create sidepanel, userpanel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import UserPanel from './UserPanel';
 
-export class SidePanel extends Component {
+class SidePanel extends Component {
   render() {
-    return <div>SidePanel goes here</div>;
+    return (
+      <Menu
+        size='large'
+        inverted
+        fixed='left'
+        vertical
+        style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
+      >
+        <UserPanel />
+      </Menu>
+    );
   }
 }
 
